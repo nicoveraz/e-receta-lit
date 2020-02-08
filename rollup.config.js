6bcb3fdab01880fdf491289aea17d612825ab455
nicoveraz <nicovera@quetru.cl>
2020-02-08 20:25:14 -0300

    add csp

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,23 @@
-//import { createDefaultConfig } from '@open-wc/building-rollup';
-
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
-
-// export default createDefaultConfig({ input: './index.html' });
 import cpy from 'rollup-plugin-cpy';
 
-const config = createCompatibilityConfig({
-  input: './index.html',
-});
-// if you use an array of configs, you don't need the copy task to be executed for both builds.
-// we can add the plugin only to the first rollup config:
-export default [
-  // add plugin to the first config
-  {
-    ...config[0],
-    plugins: [
-      ...config[0].plugins,
-      cpy([
-			{
-		    files: ['manifest.json', 'images/**'],
-		    dest: 'dist',
-		    options: {
-		      	// parents makes sure to preserve the original folder structure
-		      	parents: true,
-		    	}
-		   }
-		]),
-    ],
-  },
+import { createDefaultConfig } from '@open-wc/building-rollup';
 
-  // leave the second config untouched
-  config[1],
-];
+const config = createDefaultConfig({ input: './index.html' });
+
+export default {
+  ...config,
+  output: {
+    ...config.output,
+    sourcemap: false,
+  },
+  plugins: [...config.plugins, cpy([
+      {
+        files: ['manifest.json', 'images/**'],
+        dest: 'dist',
+        options: {
+            // parents makes sure to preserve the original folder structure
+            parents: true,
+          }
+       }
+    ])],
+};
